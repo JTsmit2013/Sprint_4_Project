6cc65bf6d51ad7ce4c31c247efe49ff4bc127bc1
JTsmit2013 <joshuaparkersmith@gmail.com>
2024-06-29 22:49:28 -0400

    Update app.py
    Commenting out a lot of code to figure out where the error is occuring when using render

--- app.py
+++ app.py
@@ -11,10 +11,10 @@ show_manuf_1k_ads = st.checkbox('Include manufacturers with less than 1000 ads')
 if not show_manuf_1k_ads:
     df = df.groupby('manufacturer').filter(lambda x: len(x) > 1000)
 
-st.dataframe(df)
-st.header('Vehicle types by manufacturer')
-st.write(px.histogram(df, x='manufacturer', color='type'))
-st.header('Histogram of `condition` vs `model_year`')
+##st.dataframe(df)
+##st.header('Vehicle types by manufacturer')
+##st.write(px.histogram(df, x='manufacturer', color='type'))
+##st.header('Histogram of `condition` vs `model_year`')
 
 # -------------------------------------------------------
 # histograms in plotly:
@@ -27,27 +27,27 @@ st.header('Histogram of `condition` vs `model_year`')
 # -------------------------------------------------------
 
 # histograms in plotly_express:
-st.write(px.histogram(df, x='model_year', color='condition'))
+##st.write(px.histogram(df, x='model_year', color='condition'))
 # a lot more concise!
 # -------------------------------------------------------
 
-st.header('Compare price distribution between manufacturers')
-manufac_list = sorted(df['manufacturer'].unique())
-manufacturer_1 = st.selectbox('Select manufacturer 1',
+##st.header('Compare price distribution between manufacturers')
+##manufac_list = sorted(df['manufacturer'].unique())
+##manufacturer_1 = st.selectbox('Select manufacturer 1',
                               manufac_list, index=manufac_list.index('chevrolet'))
 
-manufacturer_2 = st.selectbox('Select manufacturer 2',
+##manufacturer_2 = st.selectbox('Select manufacturer 2',
                               manufac_list, index=manufac_list.index('hyundai'))
-mask_filter = (df['manufacturer'] == manufacturer_1) | (df['manufacturer'] == manufacturer_2)
-df_filtered = df[mask_filter]
-normalize = st.checkbox('Normalize histogram', value=True)
-if normalize:
-    histnorm = 'percent'
-else:
-    histnorm = None
-st.write(px.histogram(df_filtered,
-                      x='price',
-                      nbins=30,
-                      color='manufacturer',
-                      histnorm=histnorm,
-                      barmode='overlay'))
+##mask_filter = (df['manufacturer'] == manufacturer_1) | (df['manufacturer'] == manufacturer_2)
+##df_filtered = df[mask_filter]
+##normalize = st.checkbox('Normalize histogram', value=True)
+##if normalize:
+##    histnorm = 'percent'
+##else:
+##    histnorm = None
+##st.write(px.histogram(df_filtered,
+##                      x='price',
+##                      nbins=30,
+##                      color='manufacturer',
+##                      histnorm=histnorm,
+##                      barmode='overlay'))
